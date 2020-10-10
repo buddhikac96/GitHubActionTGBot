b766b673b5043363accd0bb3c3d1ddf582fdd817
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-10-10 14:49:35 +0530

    worflow update

--- index.js
+++ index.js
@@ -4,68 +4,20 @@ const TelegramBot = require('node-telegram-bot-api');
 
 try {
 
-    const eventName = process.env.GITHUB_EVENT_NAME;
-    const payload = github.context.payload;
+    var payload = github.context.payload;
 
-    const tgtoken = core.getInput('TgToken');
-    const chatId = core.getInput('chatId')
+    var tgtoken = core.getInput('TgToken');
+    var chatId = core.getInput('chatId');
 
-    const bot = new TelegramBot(tgtoken);
+    var bot = new TelegramBot(tgtoken);
 
-    switch (eventName){
-        case "push":
-            pushEventAction(payload, bot, chatId);
-            break;
-        case "issue_comment":
-            issueCommentEvent(payload);
-            break;
-        case "issues":
-            issuesEvent(payload);
-            break;
-        case "fork":
-            forkEvent(payload);
-            break;
-        default:
-            noEventAction(payload);
-    }
-    
+    console.log("push event triggered");
+    console.log(payload.pusher.name);
+
+    bot.sendMessage(chatId , 'hi');
 
 } catch (error) {
 
     core.setFailed(error.message);
 
-}
-
-function pushEventAction(payload, bot, chatId){
-    console.log("push event triggered");
-    console.log(payload.pusher.name);
-
-    const output = `
-        #Push Triggered
-        By {chatId.chat.first_name}
-    `
-
-    bot.sendMessage(chatId , output , {parse_mode : "Markdown"})
-
-}
-
-
-function issueCommentEvent(payload){
-    console.log("Issue comment event triggered");
-    console.log(`Issue comment done by {payload.comment.user.login}`)
-    console.log(`Issue comment {payload.comment.body}`)
-}
-
-function issuesEvent(payload){
-    console.log("issuesEvent event triggered");
-    var chatId = core.getInput('chatId');
-}
-
-function forkEvent(payload){
-    console.log("forkEvent event triggered");
-    var chatId = core.getInput('chatId');
-}
-
-function noEventAction(payload){
-    console.log("No event triggered");
 }
