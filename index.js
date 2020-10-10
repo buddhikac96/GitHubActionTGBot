3fe401729c63b2e258eb855b497b591e8e30c8e2
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-10-10 15:38:50 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const TelegramBot = require('node-telegram-bot-api');
-
-const payload = github.context.payload;
 
 const tgtoken = core.getInput('TgToken');
 const chatId = core.getInput('chatId');
 
 const bot = new TelegramBot(tgtoken);
 
-const evresp = (gevent) => {
-    console.log(gevent);
-    return 'Hi';
-}
-
-const output = evresp(process.env.ghevent);
-bot.sendMessage(chatId, output)
+bot.sendMessage(chatId, 'hi');
