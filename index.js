ae14fa11474e97ffe6ddfca3d98cfb0c35f04d02
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-10-10 15:11:15 +0530

    update bot

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const TelegramBot = require('node-telegram-bot-api');
 const payload = github.context.payload;
 
 const tgtoken = core.getInput('TgToken');
-const chatId = core.getInput('chatId');
 
-console.log(tgtoken);
-console.log(chatId);
-
-const bot = new TelegramBot(tgtoken);
+const bot = new TelegramBot(tgtoken, {polling: true});
 
 console.log("push event triggered");
 console.log(payload.pusher.name);
 
-// bot.sendMessage(chatId , 'hi');
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(chatId, 'Received your message');
+});
